9224f2dfa49e30c57ae4a6a4f7dc4ef27992864b
Diganta Som <itsdsom111@gmail.com>
2021-04-27 14:14:03 +0530

    Added Google sign-in functionality using Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { useSelector } from 'react-redux';
+import { auth } from './firebase';
+
+import { useDispatch, useSelector } from 'react-redux';
 import { selectIsSendMessageOpen } from './features/mailSlice';
+import { login, selectUser } from './features/userSlice';
 
-import './App.css';
-
+import Login from './Login';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Mail from './Mail';
 import EmailList from './EmailList';
 import SendMail from './SendMail';
 
+import './App.css';
+
 const App = () => {
   const isSendMessageOpen = useSelector(selectIsSendMessageOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL
+          })
+        );
+      }
+    });
+  }, [dispatch]);
 
   return (
     <Router>
-      <div className='app'>
-        <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app'>
+          <Header />
 
-        <div className='app__body'>
-          <Sidebar />
+          <div className='app__body'>
+            <Sidebar />
 
-          <Switch>
-            <Route path='/mail'>
-              <Mail />
-            </Route>
-            <Route path='/'>
-              <EmailList />
-            </Route>
-          </Switch>
+            <Switch>
+              <Route path='/mail'>
+                <Mail />
+              </Route>
+              <Route path='/'>
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+
+          {isSendMessageOpen && <SendMail />}
         </div>
-
-        {isSendMessageOpen && <SendMail />}
-      </div>
+      )}
     </Router>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,3 @@
-import './Header.css';
-import constants from './constants';
-// Material UI and Icons
 import { Avatar, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
@@ -8,7 +5,23 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from './features/userSlice';
+
+import { auth } from './firebase';
+
+import constants from './constants';
+import './Header.css';
+
 const Header = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const signOut = async () => {
+    await auth.signOut();
+    dispatch(logout());
+  };
+
   return (
     <div className='header'>
       <div className='header__left'>
@@ -31,7 +44,7 @@ const Header = () => {
         <IconButton>
           <NotificationsIcon />
         </IconButton>
-        <Avatar />
+        <Avatar src={user?.photoUrl} onClick={signOut} className='header__avatar' />
       </div>
     </div>
   );
